366382209f718bc03e5d9a284c1c833b43a00954
Merari May Sosa <merarimaysosa@gmail.com>
2024-04-05 09:58:44 -0600

    01 - Primera clase, ejecucion de mensaje en consola

---
+++ src/Clase01/PrimeraClase.java
@@ -0,0 +1,7 @@
+package Clase01;
+
+public class PrimeraClase {
+    public static void main(String[] args) {
+        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!");
+    }
+}
